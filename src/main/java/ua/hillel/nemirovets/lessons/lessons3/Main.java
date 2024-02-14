3a3d59d0c9c36ff02f12b1f8bf7072c2b4738d93
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-14 13:51:08 +0200

    final

--- src/main/java/ua/hillel/nemirovets/lessons/lessons3/Main.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons3/Main.java
@@ -3,5 +3,6 @@ package ua.hillel.nemirovets.lessons.lessons3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
+        System.out.println("Hello!!!Finall");
     }
 }

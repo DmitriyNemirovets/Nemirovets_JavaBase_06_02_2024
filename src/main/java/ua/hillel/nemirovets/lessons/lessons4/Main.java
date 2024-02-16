d92e27d6b46b64cef515c1c173133d3b78045e0b
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-16 21:32:44 +0200

    lessons4

---
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons4/Main.java
@@ -0,0 +1,144 @@
+package ua.hillel.nemirovets.lessons.lessons4;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a = 3;
+//        int b =10;
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(b / a);
+//
+//
+//        System.out.println(10.0 / 3);
+//        System.out.println(10 / 3.0);
+//        System.out.println(10.0 / 3.0);
+//        System.out.println(10D / 3);
+//        System.out.println((double)b / a);
+
+
+
+
+//        System.out.println( 8 % 3);  // 2*3 = 6. 8-6=2
+//        System.out.println(9%4); //2*4=8  9-8=1
+
+
+
+
+
+
+//        System.out.println(10 % 1);
+//        System.out.println(10 % 2);
+//        System.out.println(10 % 3);
+//        System.out.println(10 % 4);
+//        System.out.println(10 % 5);
+//        System.out.println(10 % 6);
+//        System.out.println(10 % 7);
+//        System.out.println(10 % 8);
+//        System.out.println(10 % 9);
+//        System.out.println(10 % 10);
+//        System.out.println(10 % 11);
+
+
+
+//        int x  = 10;
+//        int y = x + 5;
+//        x = y;
+//        System.out.println(x);
+
+
+//        int x = 10;
+//        x = x + 5;
+//        System.out.println(x);
+
+
+//        int x =10;
+//        x += 5;
+//        x -= 5;
+//        x *= 5;
+//        x /= 5;
+//        x %= 5;
+//        System.out.println(x);
+
+     //   int x = 10;
+     // x = x + 1;
+     // x+=1;
+     // ++x;
+
+
+        //  System.out.println(x++); // постфіксний інкримент спочатку віддаємо а потім збідьшує
+        //   System.out.println(++x); // префіксний інкримент спочатку збідьшує а потім віддає
+
+//       int a = 5;
+//       int b = 2;
+//       int result = --a + ++a + ++b;  // 4 + 5 + 3
+//       int result = a++ + ++a - --b; // 5 + 7 - 1
+//       System.out.println("result = " + result);
+//       System.out.println("a = " + a);
+//       System.out.println("b = "+ b);
+
+//        System.out.println("djsjbf\t\t\tj,df,kaj.ajsf,kajs.djaskfjaskfjas\ndfjhsdkjfhs,djklshdfksdjflksdhlksdh");
+
+
+
+
+//        int absValue = Math.abs(10);
+//        System.out.println(absValue);
+//        System.out.println(Math.abs(-10));
+
+
+//        System.out.println(Math.pow(2.2,4.3));
+
+//        System.out.println(Math.sqrt(16.55));
+
+//        System.out.println(Math.round(11.1)); //11
+//        System.out.println(Math.round(11.4)); //11
+//        System.out.println(Math.round(11.5)); //12
+//        System.out.println(Math.round(11.9)); //12
+
+         //     System.out.println(Math.max(1, 5));
+        //     System.out.println(Math.min(11, 5));
+
+//        System.out.println(Math.random()); //from 0.0 to 0.999999999999999999
+
+//        System.out.println( (int) (Math.random()*11)); // from 0 to 10
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//        System.out.println( (int) (Math.random()*11));
+//
+//        System.out.println(Math.round(Math.random() *10));
+
+
+      //  int randomValue = Math.random();
+
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+//        System.out.println((int) (Math.random() *16 +15) );
+
+
+        int min =  25;
+        int max = 50;
+        int result  = (int) (Math.random()* (max-min +1) +min);
+        System.out.println(result);
+
+
+
+
+
+
+
+    }
+}

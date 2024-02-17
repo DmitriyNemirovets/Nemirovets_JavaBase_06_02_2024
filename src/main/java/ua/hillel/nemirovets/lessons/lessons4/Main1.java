9c53956db9123c668757610f251369eb1d3f63a0
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-17 09:20:34 +0200

    lessons4_main1

---
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons4/Main1.java
@@ -0,0 +1,14 @@
+package ua.hillel.nemirovets.lessons.lessons4;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int a = 21; // Length parallelepiped
+        int b = 16; // Width parallelepiped
+        int c = 13; // Height parallelepiped
+
+        int Volume = a * b * c; // Formula Volume parallelepiped
+        System.out.println("Volume of a parallelepiped = " + "" + Volume );
+        int Lenght = (4 * a) + (4 * b) + (4 * c); // Formula Lenght parallelepiped
+        System.out.println("length of all sides = " + "" + Lenght);
+    }
+}

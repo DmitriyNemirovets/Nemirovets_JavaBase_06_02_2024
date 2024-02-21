70509396f5fb2801bfff3b527c4de6a3ae80d5c4
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-21 13:04:40 +0200

    finish2 lessons 5

--- src/main/java/ua/hillel/nemirovets/lessons/lessons5/Main12.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons5/Main12.java
@@ -3,7 +3,7 @@ package ua.hillel.nemirovets.lessons.lessons5;
 import java.util.Scanner;
 
 public class Main12 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the name of the first team");
@@ -13,42 +13,42 @@ public class Main12 {
         System.out.println("Enter frags of players first team");
 
         int team1User1Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team1User1Int = scanner.nextInt();
             System.out.println("The number of frags of player 1 first team " + team1User1Int);
-        }else {
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team1User2Int = 0;
-        if (scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team1User2Int = scanner.nextInt();
             System.out.println("The number of frags of player 2 first team " + team1User2Int);
-        }else {
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team1User3Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team1User3Int = scanner.nextInt();
             System.out.println("The number of frags of player 3 first team " + team1User3Int);
-        }else{
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team1User4Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team1User4Int = scanner.nextInt();
             System.out.println("The number of frags of player 4 first team " + team1User4Int);
-        }else {
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team1User5Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team1User5Int = scanner.nextInt();
             System.out.println("The number of frags of player 5 first team " + team1User5Int);
-        }else {
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
@@ -62,63 +62,58 @@ public class Main12 {
         System.out.println("Enter frags of players second team");
 
         int team2User1Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team2User1Int = scanner.nextInt();
             System.out.println("The number of frags of player 1 second team" + team2User1Int);
-        }else{
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team2User2Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team2User2Int = scanner.nextInt();
             System.out.println("The number of frags of player 2 second team" + team2User2Int);
-        }else{
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team2User3Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team2User3Int = scanner.nextInt();
-            System.out.println("The number of frags of player 3 second team" + team2User3Int );
-        }else{
+            System.out.println("The number of frags of player 3 second team" + team2User3Int);
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team2User4Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team2User4Int = scanner.nextInt();
             System.out.println("The number of frags of player 4 second team" + team2User4Int);
-        }else{
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         int team2User5Int = 0;
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             team2User5Int = scanner.nextInt();
             System.out.println("The number of frags of player 5 second team " + team2User5Int);
-        }else{
+        } else {
             System.out.println("Wrong data. Restart application");
             System.exit(0);
         }
         scanner.nextLine();
 
-        double arithmeticAverageTeam1 = (team1User1Int + team1User2Int + team1User3Int + team1User4Int + team1User5Int)/5;
-        System.out.println();
-        double arithmaticAverageTeam2 = (team2User1Int + team2User2Int + team2User3Int + team2User4Int + team2User5Int)/5;
+        double arithmeticAverageTeam1 = (team1User1Int + team1User2Int + team1User3Int + team1User4Int + team1User5Int) / 5;
+        double arithmeticAverageTeam2 = (team2User1Int + team2User2Int + team2User3Int + team2User4Int + team2User5Int) / 5;
 
-        System.out.println("Arithmetic average  " + team1Name + " = " + arithmeticAverageTeam1);
-        System.out.println("Arithmetic average  " + team2Name + " = " + arithmaticAverageTeam2);
+//        System.out.println("Arithmetic average  " + team1Name + " = " + arithmeticAverageTeam1);
+//        System.out.println("Arithmetic average  " + team2Name + " = " + arithmeticAverageTeam2);
 
-        System.out.println();
-        System.out.println();
-
-
-        if(arithmeticAverageTeam1 > arithmaticAverageTeam2){
-        System.out.println("Winner Team 1 " +   team1Name + "=" +" " + arithmeticAverageTeam1 + " " + "points" );
-        }else if(arithmeticAverageTeam1 < arithmaticAverageTeam2){
-            System.out.println("Winner Team 2" + " " + team2Name + "=" + " " + arithmaticAverageTeam2 + " " + "points" );
-        }else{
+        if (arithmeticAverageTeam1 > arithmeticAverageTeam2) {
+            System.out.println("Winner Team 1 " + team1Name + " = " + arithmeticAverageTeam1 + " points ");
+        } else if (arithmeticAverageTeam1 < arithmeticAverageTeam2) {
+            System.out.println("Winner Team 2 " + team2Name + " = " + arithmeticAverageTeam2 + " points ");
+        } else {
             System.out.println("Team1 == Team2");
         }
     }

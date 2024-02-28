b2fdb0456c06c3d4d7ebe2ed2ed5139082831f2e
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-28 10:47:20 +0200

    add home work demo

--- src/main/java/ua/hillel/nemirovets/lessons/lessons7/Demo1.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons7/Demo1.java
@@ -8,7 +8,7 @@ public class Demo1 {
 
         int[] team1 = new int[25];
         int[] team2 = new int[25];
-//
+
         double sumTeam1 = 0.0;
         double sumTeam2 = 0D;
 
@@ -16,23 +16,21 @@ public class Demo1 {
             int minOld = 18;
             int maxOld = 40;
             maxOld -= minOld;
-            int rezultTeam1Old  = (int)(Math.random() * ++maxOld) + minOld ;
-            team1[i] = (rezultTeam1Old + 1) ;
+            int rezultTeam1Old = (int) (Math.random() * ++maxOld) + minOld;
+            team1[i] = (rezultTeam1Old + 1);
             sumTeam1 += team1[i];
-
         }
         for (int i = 0; i < team2.length; i++) {
             int minOld = 18;
             int maxOld = 40;
             maxOld -= minOld;
-            int rezultTeam2Old = (int)(Math.random()* ++maxOld) + minOld;
-            team2[i] = (rezultTeam2Old +1);
+            int rezultTeam2Old = (int) (Math.random() * ++maxOld) + minOld;
+            team2[i] = (rezultTeam2Old + 1);
             sumTeam2 += team2[i];
         }
-
-        System.out.println(Arrays.toString  (team1));
+        System.out.println(Arrays.toString(team1));
         System.out.println(Arrays.toString(team2));
-        System.out.println("Average age of players One Team = " + sumTeam1 / team1.length );
+        System.out.println("Average age of players One Team = " + sumTeam1 / team1.length);
         System.out.println("Average age of players Two Team = " + sumTeam2 / team2.length);
     }
 }

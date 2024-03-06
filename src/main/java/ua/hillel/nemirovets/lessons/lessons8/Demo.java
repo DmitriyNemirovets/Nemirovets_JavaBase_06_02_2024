2bbd0836ccee58985aa812a65ce1cd844070e837
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-06 10:57:41 +0200

    add demo files1 homework

--- src/main/java/ua/hillel/nemirovets/lessons/lessons8/Demo.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons8/Demo.java
@@ -12,13 +12,9 @@ public class Demo {
         int minValue = 0;
         int maxValue = 9;
 
-        double sum1 = 0.0;
-        double sum2 = 0.0;
-
-               for (int i = 0; i < lottery.length; i++) {
+        for (int i = 0; i < lottery.length; i++) {
             int result1 = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
             lottery[i] = result1;
-            sum1 += lottery[i];
             int key = lottery[i];
             int j = i - 1;
             while (j >= 0 && lottery[j] > key) {
@@ -31,19 +27,15 @@ public class Demo {
         for (int i = 0; i < player.length; i++) {
             int result = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
             player[i] = result;
-            sum2 += player[i];
             int key = player[i];
             int j = i - 1;
-            while (j >= 0 && player[j] > key){
-                player[j + 1] =  player[j];
+            while (j >= 0 && player[j] > key) {
+                player[j + 1] = player[j];
                 j--;
             }
             player[j + 1] = key;
         }
-
         System.out.println(Arrays.toString(lottery));
         System.out.println(Arrays.toString(player));
-
-
     }
 }

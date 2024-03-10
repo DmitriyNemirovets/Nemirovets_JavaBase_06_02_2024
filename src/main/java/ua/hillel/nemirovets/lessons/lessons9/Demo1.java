fcfaa4d70e8d0dcc6ba30f934d6823c371728105
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-10 21:04:41 +0200

    add  files Transpont11

--- src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo1.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo1.java
@@ -30,13 +30,13 @@ public class Demo1 {
             }
         }
 
-        System.out.println("AFTER");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
+//        System.out.println("AFTER");
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                System.out.print(array[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
 
 
     }
---
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo2.java
@@ -0,0 +1,94 @@
+package ua.hillel.nemirovets.lessons.lessons9;
+
+import java.util.Scanner;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int m = 0;
+        int n = 0;
+
+        System.out.println("Enter Value m : ");
+        while (scanner.hasNextLine()) {
+            if (scanner.hasNextInt()) {
+                int temp = scanner.nextInt();
+                if (temp < 0 || temp > 10) {
+                    System.out.println("Value must be in range of 0 to 10");
+                }else{
+                    m = temp;
+                    System.out.println("m = " + m);
+                    break;
+                }
+            } else {
+                System.out.println("Wrong data please enter number");
+            }
+            scanner.nextLine();
+        }
+        scanner.nextLine();
+        System.out.println("Enter Value n : ");
+        while (scanner.hasNextLine()) {
+            if (scanner.hasNextInt()) {
+                int temp = scanner.nextInt();
+                if (temp < 0 || temp > 10) {
+                    System.out.println("Value must be in range of 0 to 10");
+                }else{
+                    n = temp;
+                    System.out.println("n = " + n);
+                    break;
+                }
+            } else {
+                System.out.println("Wrong data please enter number");
+            }
+            scanner.nextLine();
+        }
+
+
+        int[][] array1 = new int[m][n];
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        System.out.println("Matrix 1 ");
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1[i].length; j++) {
+                System.out.print(array1[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        int[][] array2 = new int [n][m];
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array1[j][i];
+
+            }
+        }
+        System.out.println("Matrix 2 ");
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int[][] array1 = new int[m][n];
+//        int[][] array2 = new int[n][m];
+

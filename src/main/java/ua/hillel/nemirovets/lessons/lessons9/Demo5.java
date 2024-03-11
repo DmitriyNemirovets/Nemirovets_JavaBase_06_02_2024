ffd8af7aea0221820927dd3705cae1cc343d32c0
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-11 14:18:21 +0200

    add  files Transpont11

---
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo5.java
@@ -0,0 +1,29 @@
+package ua.hillel.nemirovets.lessons.lessons9;
+
+import java.util.Scanner;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value");
+        int sum = 0;
+        while (sum <= 1000) {
+            if(scanner.hasNextInt()){
+                System.out.println("Enter Value : ");
+                int temp = scanner.nextInt();
+                if(temp >= 0 && temp <= 100){
+                    sum += temp;
+                }else{
+                    System.out.println("Enter Value 0 to 100");
+                }
+            }else{
+                System.out.println("Wrong Data. Enter Value");
+            }
+            scanner.nextLine();
+        }
+        System.out.println("Sum Value > 1000 ");
+        System.out.println("Sum Value = " + sum);
+
+        }
+    }
+

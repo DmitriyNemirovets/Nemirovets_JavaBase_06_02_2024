c9ac2cf8fc959b89d4703ea9705e70dbed3728cd
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-12 10:48:15 +0200

    add  files12

--- src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo10.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo10.java
@@ -2,7 +2,7 @@ package ua.hillel.nemirovets.lessons.lessons9;
 
 public class Demo10 {
     public static void main(String[] args) {
-        int[][]  array = {
+        int[][] array = {
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}
@@ -10,9 +10,9 @@ public class Demo10 {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + "\t");
-                }
-                System.out.println();
             }
+            System.out.println();
         }
     }
+}
 

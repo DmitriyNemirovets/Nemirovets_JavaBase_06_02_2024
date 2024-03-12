9ee619e87653799795d5959570df30b07c085b86
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-12 10:48:30 +0200

    add  files12

--- src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo8.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons9/Demo8.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class Demo8 {
     public static void main(String[] args) {
-        int [] array = new int[6];
+        int[] array = new int[6];
 
         int minValue = 0;
         int maxValue = 0;
 
-        for (int i = 0; i < array.length ; i++) {
-            int result = (int)(Math.random() * 10 + 1 );
+        for (int i = 0; i < array.length; i++) {
+            int result = (int) (Math.random() * 10 + 1);
             array[i] = result;
             int key = array[i];
             int j = i - 1;
-            while (j >= 0 && array[j] > key){
+            while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j--;
             }

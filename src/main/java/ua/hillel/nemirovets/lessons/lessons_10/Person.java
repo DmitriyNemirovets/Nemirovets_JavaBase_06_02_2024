6cec91b237553033a978df4562553d812de581d8
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-13 10:36:56 +0200

    add  files1

--- src/main/java/ua/hillel/nemirovets/lessons/lessons_10/Person.java
+++ src/main/java/ua/hillel/nemirovets/lessons/lessons_10/Person.java
@@ -2,16 +2,14 @@ package ua.hillel.nemirovets.lessons.lessons_10;
 
 public class Person {
     public static void main(String[] args) {
-        personInfo("Will", "Smith", "New York ", "2936729462846");
-        System.out.println();
-        personInfo("Jackie ", "Chan", "Shanghai", "12312412412");
-        System.out.println();
-        personInfo("Sherlock ", "Holmes", "London ", "37742123513");
+        Person person = new Person();
 
+        System.out.println(person.personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(person.personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(person.personInfo("Sherlock", "Holmes", "London", "37742123513"));
     }
 
-    static void personInfo(String name, String surname, String town, String tel) {
-        System.out.println("Call a citizen " + name + " " + surname + " in  town " + town + "you can by number " + tel);
-
+    public String personInfo(String name, String surname, String city, String phone) {
+        return "Call a citizen " + name + " " + surname + " in  town " + city + " you can by number " + phone + ".";
     }
 }
